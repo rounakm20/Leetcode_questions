a4a3653127c34f88c2b23c4ccd981282042bdb06
Rounak Mishra <rounakm5886@gmail.com>
2026-05-04 23:20:37 +0530

    Add rotate method for 90-degree matrix rotation
    Implement in-place rotation of a matrix by 90 degrees clockwise.

---
+++ Leetcode_questions/rotate-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int m = matrix.length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = i; j < m; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            int left = 0, right = m - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
