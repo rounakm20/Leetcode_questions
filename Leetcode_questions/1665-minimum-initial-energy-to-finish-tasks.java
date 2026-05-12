8c828676c4d2f8de3c16612fdd3227e06cab01ee
Rounak Mishra <rounakm5886@gmail.com>
2026-05-12 22:16:43 +0530

    Implement minimumEffort method to calculate energy

---
+++ Leetcode_questions/1665-minimum-initial-energy-to-finish-tasks.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimumEffort(int[][] tasks) {
+
+        Arrays.sort(tasks, (a, b) ->
+            (b[1] - b[0]) - (a[1] - a[0])
+        );
+
+        int energy = 0;
+        int current = 0;
+
+        for (int[] t : tasks) {
+
+            if (current < t[1]) {
+                energy += t[1] - current;
+                current = t[1];
+            }
+
+            current -= t[0];
+        }
+
+        return energy;
+    }
+}
